3e2ea557597cdf815afddebc151560fbd2aa6c89
Stefan Reeves <stefanreeves24@gmail.com>
2025-06-27 11:34:28 -0700

    add hero image

--- index.php
+++ index.php
@@ -5,20 +5,25 @@
 <div class="content-container">
     <div class="content-wrapper">
         <div class="main-content">
-            <img src="images/index-1.jpg" alt="Hedden Chong office building">
+            <img src="images/homepage-hero-hedden-chong.jpg" alt="Hedden Chong Hero Image">
 
             <p>Hedden Chong LLP is a Chartered Professional Accounting firm that prides itself on providing quality and
                 dependable service.</p>
 
-            <p>Client focused and results driven, Hedden Chong LLP has been answering accounting, audit, tax and business
+            <p>Client focused and results driven, Hedden Chong LLP has been answering accounting, audit, tax and
+                business
                 consulting needs of our clients base for over 50 years.</p>
 
             <p>Our growing firm of professionals consists of a practice with over 25 members.</p>
 
-            <p>Our knowledge and understanding of business from management's perspective sets us apart as problem solvers in
+            <p>Our knowledge and understanding of business from management's perspective sets us apart as problem
+                solvers in
                 this complex global environment.</p>
 
-            <img src="images/CPA-BC-300x127.jpg" alt="Chartered Professional Accountants of British Columbia">
+            <div class="image-container">
+
+                <img src="images/CPA-BC-300x127.jpg" alt="Chartered Professional Accountants of British Columbia">
+            </div>
         </div>
 
         <div class="sidebar">

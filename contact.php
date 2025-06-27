f720529fce01352ff9b6dd4ab81d74b181427bd6
Stefan Reeves <stefanreeves24@gmail.com>
2025-06-27 14:38:22 -0700

    google map mobile responsiveness

--- contact.php
+++ contact.php
@@ -31,10 +31,11 @@
             </ul>
 
             <p><strong>Map:</strong></p>
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2603.638271094077!2d-123.01926562351207!3d49.264301771389995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548673e7bde5acdf%3A0xe0dd07f1a07a6f9d!2sHedden%20Chong%20LLP!5e0!3m2!1sen!2sca!4v1750975300519!5m2!1sen!2sca"
-                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <div class="map-container">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2603.638271094077!2d-123.01926562351207!3d49.264301771389995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x548673e7bde5acdf%3A0xe0dd07f1a07a6f9d!2sHedden%20Chong%20LLP!5e0!3m2!1sen!2sca!4v1750975300519!5m2!1sen!2sca"
+                    allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
         </div>
 
         <div class="sidebar">
